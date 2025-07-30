95d6d0ddd7d2f128fd42947f48cf5f296abced7a
Nityanand <you@example.com>
2025-07-30 15:22:14 +0530

    fixes some issues

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     mongoose.connection.on("connected", () =>
       console.log("database connected")
     );
-    await mongoose.connect(`${process.env.MONGODB_URL}quickblog`);
+    await mongoose.connect(`${process.env.MONGODB_URL}/quickblog`);
   } catch (error) {
     console.log(error.message);
   }
